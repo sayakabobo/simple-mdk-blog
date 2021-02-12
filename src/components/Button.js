c1703f869e96e116b4457e5f3bed7df0dc65aca7
sayakabobo <muchochokogooo+it@gmail.com>
2021-02-13 01:29:24 +0900

    Create a Content Crad components

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { ButtonWrapper } from "../elements"
+
+export const Button = ({ children, href }) => {
+  return (
+    <div>
+      <ButtonWrapper to={href}>{children}</ButtonWrapper>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import React from "react"
-import { Container, FeatureImage } from "../components"
+import { Container, FeatureImage, Content, ContentCard } from "../components"
 
 const IndexPage = () => {
   return (
     <Container>
       <FeatureImage />
+      <Content>
+        <ContentCard
+          date="2021-02-13"
+          title="testtest"
+          except="LoremLorem"
+          slug="/test"
+        />
+      </Content>
     </Container>
   )
 }
